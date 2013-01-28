022681449dcaa8a64eb49c0d846975312fff0826
Claudio Bonati <claudio.bonati82@gmail.com>
2013-01-28 20:01:50 +0100

    version 0.6

--- partkill.py
+++ partkill.py
@@ -9,7 +9,7 @@
 # century and look it up online!
 #
 
-__version__ = "0.5"
+__version__ = "0.6"
 
 from email.mime.text import MIMEText
 from logging import basicConfig, debug, info, warning, error ,critical, \
